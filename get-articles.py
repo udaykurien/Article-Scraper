97af32f47bf993fafd86f32cff546915f24b9b4a
UK <udaykurien.dev@gmail.com>
2022-11-17 04:05:58 -0500

    Added New Yorker and options menu.

--- get-articles.py
+++ get-articles.py
@@ -1,26 +1,36 @@
 import cloudscraper
 from bs4 import BeautifulSoup
 
-# Journal options:
-# 1. New York Times
-# 2. The Economist
+menu = '# Journal options:\n\
+# 0. Quit\n\
+# 1. New York Times\n\
+# 2. The Economist\n\
+# 3. New Yorker'
 
-jrnl_optn = 3
+print(menu)
 
-url = 'https://www.nytimes.com/2022/10/26/opinion/grief-death-lessons-on-living.html'
-url = 'https://www.economist.com/graphic-detail/2022/11/14/a-rare-reason-for-optimism-about-climate-change'
+repeat = True
+while repeat:
+  repeat = False
+  jrnl_optn = int(input("Enter journal option: "))
+  if jrnl_optn == 1:
+    attributes = {'class':'css-at9mc1 evys1bk0'}
+  elif jrnl_optn == 2:
+    attributes = {'class':'article__body-text'}
+  elif jrnl_optn == 3:
+    attributes = {'class':'paywall'}
+  elif jrnl_optn ==0:
+    quit()
+  else:
+    print("Invalid selection")
+    repeat = True
+
+url = input("Enter article url: ")
 
 fname = url.split('/')[-1].split('.html')[0]
 floc = 'Articles'
 fpath = floc + '/' + fname
 
-if jrnl_optn == 1:
-  attributes = {'class':'css-at9mc1 evys1bk0'}
-elif jrnl_optn == 2:
-  attributes = {'class': 'article__body-text'}
-else:
-  print("Invalid selection")
-  quit()
 
 scraper = cloudscraper.create_scraper()
 response = scraper.get(url)
@@ -36,3 +46,5 @@ with open(fpath, 'w') as f:
     f.write("\n")
 
 
+#url = 'https://www.nytimes.com/2022/10/26/opinion/grief-death-lessons-on-living.html'
+#url = 'https://www.economist.com/graphic-detail/2022/11/14/a-rare-reason-for-optimism-about-climate-change'
